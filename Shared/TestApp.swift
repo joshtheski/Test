3c20a104af2a0bb62b54c14792ac950a89f743fb
Joshua Gajewski <gajewski.j@icloud.com>
2021-10-26 21:19:18 -0400

    Initial Commit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Joshua on 2021-10-26.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
